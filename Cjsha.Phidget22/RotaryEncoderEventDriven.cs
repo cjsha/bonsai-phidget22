ee3485fde311873caa5be16edf1130678ce65632
cjsha <cs@open-ephys.org>
2025-05-28 16:54:40 -0400

    Make polling & event-driven nodes for rotary encoder

---
+++ Cjsha.Phidget22/RotaryEncoderEventDriven.cs
@@ -0,0 +1,38 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using Phidget22;
+using Phidget22.Events;
+
+namespace Cjsha.Phidget22
+{
+    [Description("")]
+    [Combinator(MethodName = nameof(Generate))]
+    [WorkflowElementCategory(ElementCategory.Source)]
+    public class RotaryEncoderEventDriven
+    {
+        [Description("")]
+        [Range(1, 1000)]
+        public int PositionChangeTrigger { get; set; } = 1;
+
+        [Description("")]
+        [Range(0, 5)]
+        public int Channel { get; set; } = 0;
+
+        public IObservable<long> Generate()
+        {
+            Encoder encoder = new Encoder{ Channel = Channel };
+            encoder.Open(Phidget.DefaultTimeout);
+            encoder.PositionChangeTrigger = PositionChangeTrigger;
+
+            return Observable.FromEventPattern<EncoderPositionChangeEventHandler, EncoderPositionChangeEventArgs>
+            (
+                h => encoder.PositionChange += h,
+                h => encoder.PositionChange -= h
+            )
+            .Select(encoderEvent => ((Encoder)encoderEvent.Sender).Position)
+            .Finally(() => encoder.Close());
+        }
+    }
+}
---
+++ Cjsha.Phidget22/RotaryEncoderPolling.cs
@@ -0,0 +1,37 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using Phidget22;
+using Phidget22.Events;
+
+namespace Cjsha.Phidget22
+{
+    [Description("")]
+    [Combinator(MethodName = nameof(Generate))]
+    [WorkflowElementCategory(ElementCategory.Source)]
+    public class RotaryEncoderPolling
+    {
+        [Description("")]
+        public float SampleFrequencyHz { get; set; } = 100;
+
+        [Description("")]
+        [Range(0, 5)]
+        public int Channel { get; set; } = 0;
+
+        public IObservable<long> Generate()
+        {
+            Encoder encoder = new Encoder { Channel = Channel };
+            encoder.Open(Phidget.DefaultTimeout);
+            encoder.DataRate = SampleFrequencyHz;
+
+            return Observable.FromEventPattern<EncoderPositionChangeEventHandler, EncoderPositionChangeEventArgs>
+            (
+                h => encoder.PositionChange += h,
+                h => encoder.PositionChange -= h
+            )
+            .Select(encoderEvent => ((Encoder)encoderEvent.Sender).Position)
+            .Finally(() => encoder.Close());
+        }
+    }
+}
